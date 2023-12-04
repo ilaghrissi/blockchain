76bed3da82856ac5d18b1803c7e79457bcda1d94
LAGHRISSI Imad <laghrissi.imad@gmail.com>
2023-12-04 10:01:36 +0100

    first commit

---
+++ src/main/kotlin/com/example/blockchain/BlockchainApplication.kt
@@ -0,0 +1,11 @@
+package com.example.blockchain
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlockchainApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlockchainApplication>(*args)
+}
